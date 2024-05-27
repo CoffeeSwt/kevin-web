61b1b08e370d790731f2af5af02e0de5ddab0ce6
Coffeesw <897884964@qq.com>
2024-05-27 18:32:33 +0800

    ✨ feat: 页面布局

--- web/src/views/GameDetail.vue
+++ web/src/views/GameDetail.vue
@@ -2,7 +2,7 @@
     <div ref="scrollWrapperRef" bg-main-bg h-screen color-white overflow-hidden tracking-widest>
         <div size-full overflow-scroll scroll-smooth>
             <!-- Phone Mode -->
-            <div v-if="!lgMode" p-4 flex flex-col box-border>
+            <div p-4 flex flex-col box-border>
                 <!-- logo -->
                 <div h-16 flex-shrink-0>
                     <img h-full :src="game.logo" alt="">
@@ -10,7 +10,7 @@
                 <!-- theme name -->
                 <div flex gap-4 my-4 flex-shrink-0>
                     <template v-for="(theme, index) in game.theme">
-                        <div text-deep-gray bg-black-l-1 p-2 rounded-lg class="min-w-1/3"
+                        <div text-deep-gray bg-black-l-1 cursor-pointer p-2 px-4 rounded-lg lg:w-40
                             :class="{ 'theme-high-light': currentThemeIndex == index }"
                             @click="handleThemeChange(index)">
                             <div text-lg font-700>
@@ -22,80 +22,88 @@
                         </div>
                     </template>
                 </div>
-                <!-- imgs -->
-                <div w-full aspect-video flex-shrink-0>
-                    <!-- Slider main container -->
-                    <div class="swiper" size-full>
-                        <!-- Additional required wrapper -->
-                        <div class="swiper-wrapper" size-full>
-                            <!-- Slides -->
-                            <template v-for="img in currentTheme.imgs">
-                                <div class="swiper-slide" rounded-lg overflow-hidden>
-                                    <img size-full :src="img" alt="">
-                                </div>
-                            </template>
-                        </div>
-                        <!-- If we need pagination -->
-                        <!-- <div class="swiper-pagination"></div> -->
-
-                        <!-- If we need navigation buttons -->
-                        <!-- <div class="swiper-button-prev"></div> -->
-                        <!-- <div class="swiper-button-next"></div> -->
-
-                        <!-- If we need scrollbar -->
-                        <div class="swiper-scrollbar"></div>
-                    </div>
-                </div>
-                <div w-full overflow-hidden mt-2 flex-shrink-0>
-                    <div w-full overflow-x-auto overflow-y-hidden flex gap-1>
-                        <template v-for="(img, index) in currentTheme.imgs">
-                            <div :class="{ 'high-light': currentPicIndex == index }" h-14 aspect-video rounded-lg
-                                box-border @click="picClickHandler(index)">
-                                <img size-full rounded-lg :src="img" alt="">
-                            </div>
-                        </template>
-                    </div>
-                </div>
-
-                <!-- custom theme parts change -->
-                <div text-white flex-grow-1 w-full flex flex-col box-border mt-4>
-                    <!-- menu part -->
-                    <div flex gap-2 flex-shrink-0 w-full>
-                        <template v-for="(tag, index) in currentTheme.tags">
-                            <div :class="{ 'active-tag-menu': currentThemeTagIndex == index }" duration-100 p-3
-                                @click="changCurrentThemeTagIndex(index)" rounded-t-lg text-nowrap>
-                                {{ tag.cnName }}
-                            </div>
-                        </template>
-                    </div>
-                    <!-- content part -->
-                    <div flex-grow-1 w-full overflow-y-hidden box-border class="active-tag" flex flex-col p-3>
-                        <div h-0 flex-shrink-0></div>
-                        <!-- text type -->
-                        <div v-if="!tagContent.ifArrayContent" w-full box-border flex-grow-1 overflow-y-auto
-                            whitespace-pre-wrap>
-                            {{ tagContent.content }}
-                        </div>
-                        <!-- array type -->
-                        <div v-else w-full box-border flex-grow-1 overflow-y-auto whitespace-pre-wrap>
-                            <div v-if="tagContent.name == 'creators'" grid w-full grid-cols-4 gap-2>
-                                <template v-for="creator in (tagContent.content as Array<{
-                                    name: string;
-                                    avator: string;
-                                    info: string;
-                                }>) ">
-                                    <div class="w-full" flex flex-col items-center bg-black-l-1 py-3 rounded-lg>
-                                        <div size-10 mb-1>
-                                            <img size-full rounded-full :src="creator.avator" alt="">
+                <div lg:flex lg:gap-8>
+                    <!-- imgs -->
+                    <div class="lg:w-2/5">
+                        <div w-full aspect-video flex-shrink-0>
+                            <!-- Slider main container -->
+                            <div class="swiper" size-full>
+                                <!-- Additional required wrapper -->
+                                <div class="swiper-wrapper" size-full>
+                                    <!-- Slides -->
+                                    <template v-for="img in currentTheme.imgs">
+                                        <div class="swiper-slide" rounded-lg overflow-hidden>
+                                            <img size-full :src="img" alt="">
                                         </div>
-                                        <div text-base mb-0.5>{{ creator.name }}</div>
-                                        <div text-xs>{{ creator.info }}</div>
+                                    </template>
+                                </div>
+                                <!-- If we need pagination -->
+                                <!-- <div class="swiper-pagination"></div> -->
+
+                                <!-- If we need navigation buttons -->
+                                <!-- <div class="swiper-button-prev"></div> -->
+                                <!-- <div class="swiper-button-next"></div> -->
+
+                                <!-- If we need scrollbar -->
+                                <!-- <div class="swiper-scrollbar"></div> -->
+                            </div>
+                        </div>
+                        <div w-full overflow-hidden mt-2 flex-shrink-0>
+                            <div w-full overflow-x-auto overflow-y-hidden flex gap-1>
+                                <template v-for="(img, index) in currentTheme.imgs">
+                                    <div :class="{ 'high-light': currentPicIndex == index }" h-14 aspect-video
+                                        rounded-lg box-border @click="picClickHandler(index)" cursor-pointer>
+                                        <img size-full rounded-lg :src="img" alt="">
                                     </div>
                                 </template>
                             </div>
                         </div>
                     </div>
+
+                    <!-- custom theme parts change -->
+                    <div text-white flex-grow-1 w-full flex flex-col box-border mt-4>
+                        <!-- menu part -->
+                        <div flex gap-2 flex-shrink-0 w-full>
+                            <template v-for="(tag, index) in currentTheme.tags">
+                                <div cursor-pointer :class="{ 'active-tag-menu': currentThemeTagIndex == index }" duration-100 p-3
+                                    @click="changCurrentThemeTagIndex(index)" rounded-t-lg text-nowrap>
+                                    {{ tag.cnName }}
+                                </div>
+                            </template>
+                        </div>
+                        <!-- content part -->
+                        <div flex-grow-1 w-full overflow-y-hidden box-border class="active-tag" flex flex-col p-3>
+                            <div h-0 flex-shrink-0></div>
+                            <!-- text type -->
+                            <div v-if="!tagContent.ifArrayContent" w-full box-border flex-grow-1 overflow-y-auto
+                                whitespace-pre-wrap>
+                                {{ tagContent.content }}
+                            </div>
+                            <!-- array type -->
+                            <div v-else w-full box-border flex-grow-1 overflow-y-auto whitespace-pre-wrap>
+                                <div v-if="tagContent.name == 'creators'" grid w-full grid-cols-4 gap-2 lg:grid-cols-10>
+                                    <template v-for="creator in (tagContent.content as Array<{
+                                        name: string;
+                                        avator: string;
+                                        info: string;
+                                    }>) ">
+                                        <div class="w-full" flex flex-col items-center bg-black-l-1 py-3 rounded-lg>
+                                            <div size-10 mb-1>
+                                                <img size-full rounded-full :src="creator.avator" alt="">
+                                            </div>
+                                            <div text-base mb-0.5>{{ creator.name }}</div>
+                                            <div text-xs>{{ creator.info }}</div>
+                                        </div>
+                                    </template>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+
+
+
             </div>
         </div>
     </div>
@@ -156,7 +164,7 @@ const init = () => {
         // Optional parameters
         direction: 'horizontal',
         loop: false,
-        // autoplay: true,
+        autoplay: true,
         // If we need pagination
         // pagination: {
         //     el: '.swiper-pagination',
